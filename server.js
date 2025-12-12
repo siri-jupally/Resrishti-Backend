9d2108b2d7725a02d37b43f514bc7d78eae51346
Siri Jupally <jupallysiri.04@gmail.com>
2025-12-12 22:40:55 +0530

    port change1

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
