a482a44e028338a2b0103f71625842f8b6b6155a
suryatejarampuram <suryarampuram121@gmail.com>
2026-01-28 00:22:40 +0530

    socket#2

--- server.js
+++ server.js
@@ -180,6 +180,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // test change
